5e83a3d8acf37fd3a2fb93583b84216072ca3a20
liceyoung <aliceduong841@gmail.com>
2024-06-29 12:37:57 -0500

    add template data file to test est API

---
+++ backend/template.ts
@@ -0,0 +1,22 @@
+module.exports = {
+    1 :{
+        template_id : '1',
+		name : 'blue_flower',
+		color: '#583192'
+},
+
+    2 :{
+
+
+        template_id: '2',
+		name : 'green_ocean',
+		color: '#12a0d2'
+},
+
+    3 :{
+        template_id: '3',
+        name : 'everlasting_garden',
+        color: ' #5c1230'
+},
+
+}
